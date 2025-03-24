64124cdaac534d1911d1dd09f97f87abec2f1005
grishavanika <grish.vanika@gmail.com>
2025-03-24 20:05:10 +0100

    replace glm with vcpkg #2

--- third_party/glm_integration.cmake
+++ third_party/glm_integration.cmake
@@ -1,23 +1,8 @@
 add_library(glm_Interface INTERFACE)
 
-FetchContent_Declare(
-    glm_content
-    SOURCE_DIR "${CMAKE_CURRENT_SOURCE_DIR}/deps/glm-src"
-    FULLY_DISCONNECTED ON)
-FetchContent_GetProperties(glm_content)
-if (NOT glm_content_POPULATED)
-    FetchContent_Populate(glm_content)
-endif ()
-
-if (MSVC)
-    target_compile_options(glm_Interface INTERFACE
-        # nonstandard extension used: nameless struct/union
-        /wd4201
-        )
-endif ()
+find_package(glm CONFIG REQUIRED)
 
 target_compile_definitions(glm_Interface
   INTERFACE GLM_ENABLE_EXPERIMENTAL)
 
-add_subdirectory(${glm_content_SOURCE_DIR} ${glm_content_BINARY_DIR} EXCLUDE_FROM_ALL)
-target_link_libraries(glm_Interface INTERFACE glm)
+target_link_libraries(glm_Interface INTERFACE glm::glm-header-only)
