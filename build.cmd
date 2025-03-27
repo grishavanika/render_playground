639ab93b36f7d1e647a01b02b940c9042b0c17f8
gvanika <grish.vanika@gmail.com>
2025-03-27 15:20:37 +0200

    update sample

--- build.cmd
+++ build.cmd
@@ -1,8 +1,9 @@
+set VCPKG_ROOT=C:\Users\grish\dev\vcpkg
 call cmake -S . -B build ^
-	-DCMAKE_TOOLCHAIN_FILE=C:\Users\grish\dev\vcpkg\scripts\buildsystems\vcpkg.cmake
+	-DCMAKE_TOOLCHAIN_FILE=%VCPKG_ROOT%\scripts\buildsystems\vcpkg.cmake
 call cmake -S . -B build_clang ^
 	-T ClangCL ^
-	-DCMAKE_TOOLCHAIN_FILE=C:\Users\grish\dev\vcpkg\scripts\buildsystems\vcpkg.cmake
+	-DCMAKE_TOOLCHAIN_FILE=%VCPKG_ROOT%\scripts\buildsystems\vcpkg.cmake
 
 :: call cmake --build build --config Release
 :: call cmake --build build_clang --config Release
